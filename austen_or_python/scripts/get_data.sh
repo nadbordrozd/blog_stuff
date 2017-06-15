c03b3c62da3e1e3fe117ffbea81815d848954405
nadbor <nadbordrozd@gmail.com>
2017-06-15 23:37:10 +0100

    removed austen-python thing

--- austen_or_python/scripts/get_data.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-mkdir -p ../data/austen
-aws s3 cp s3://neuralnets/blstm_blog/data/austen.txt ../data/austen/austen.txt
-
-cd ../data/
-git clone https://github.com/scikit-learn/scikit-learn.git
